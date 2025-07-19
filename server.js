f70f21b3fbb6e2718497291c9e0ff77389afb32a
Nolan Huang <nolan1027huang2008@gmail.com>
2025-07-19 12:54:44 -0700

    openai env fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { OpenAI } = require("openai");
-require('dotenv').config();
+//require('dotenv').config();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const express = require('express');
